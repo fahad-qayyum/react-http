5e8b5834843445e9fba60eb95e98892b04643f85
Fahad Qayyum <fahadq@my.yorku.ca>
2021-04-21 02:19:49 +0500

    Post and Get request with Axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,24 +1,54 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+
+    state = {
+        posts: [],
+        selectedPost: null
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPost = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Fahad'
+                    }
+                })
+                this.setState({
+                    posts: updatedPost
+                })
+            });
+    }
+
+    selectPostHandler = (id) => {
+        this.setState({
+            selectedPost: id
+        })
+    }
+
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post title={post.title} key={post.id} author={post.author}
+                         clicked={this.selectPostHandler.bind(this, post.id)}/>;
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPost}/>
                 </section>
                 <section>
-                    <NewPost />
+                    <NewPost/>
                 </section>
             </div>
         );
